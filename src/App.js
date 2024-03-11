f1682d05512b25f780ac93841d300c90eddb3518
rafidulislam19 <rafidul.islam19@northsouth.edu>
2024-03-12 04:15:33 +0600

    Load More Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordian from "./components/accordian";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -10,11 +11,12 @@ function App() {
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
       {/* <StarRating noOfStars={10} /> */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+      <LoadMoreData />
     </div>
   );
 }
